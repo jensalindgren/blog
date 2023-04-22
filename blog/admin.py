eb87956fff1b6b59bf52198f506b8ac80400da9d
Jens Lindgren <jens_lindgren93@hotmail.com>
2023-04-22 15:24:02 +0000

    Had trouble with my data base i flushed it added new. superuser and it worked.

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Post)
+class postAdmin(SummernoteModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    summernote_fields = ('content',)
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ('title', 'content')
+    action = ('approve_post')
+
+    def approve_post(self, request, queryset):
+        queryset.update(status=1)
+
+
+@admin.register(Comment)
+class commentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ('approve_comments')
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+# A model for the blog posts
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='blog_post'
+    )
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=1)
+    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+    def number_of_comments(self):
+        return self.comments.count()
+
+
+# A model for the comments
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=True)
+    likes = models.ManyToManyField(User, related_name='comment_likes',
+                                   blank=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
+
+    def number_of_likes(self):
+        return self.likes.count()
