fccfe73b8ddf68fd15e3aba51dda08ae63e44497
Jens Lindgren <jens_lindgren93@hotmail.com>
2023-04-22 16:51:00 +0000

    Added some basic structure to the blog app.

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Post, Comment
-
-# Create your views here.
 
 
 class PostList(generic.ListView):
@@ -10,3 +8,27 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by("-created_on")
     template_name = "index.html"
     paginate_by = 6
+
+
+class PostDetail(View):
+    """ Get post to be added and render a form to add comments """
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('-created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            'post_detail.html',
+            {
+                'post': post,
+                'comments': comments,
+                'commented': False,
+                'liked': liked,
+                'comment_form': CommentForm()
+            },
+        )
