8cf62a6fc7f2a350852644bd3359f7019a9b2f73
Jens Lindgren <jens_lindgren93@hotmail.com>
2023-04-23 09:06:24 +0000

    Having trouble displaying crispy forms in my django project

---
+++ blog/forms.py
@@ -0,0 +1,23 @@
+from .models import Post, Comment
+from django import forms
+from django_summernote.widgets import SummernoteWidget
+
+
+class CommentForm(forms.ModelForm):
+    """ Form for Comments submission """
+    class Meta:
+        model = Comment
+        fields = ['body',]
+        widgets = {
+            'content': SummernoteWidget(),
+        }
+
+
+class PostForm(forms.ModelForm):
+    """ Form for Post submission """
+    class Meta:
+        model = Post
+        fields = ['title', 'content',]
+        widgets = {
+            'content': SummernoteWidget(),
+        }
